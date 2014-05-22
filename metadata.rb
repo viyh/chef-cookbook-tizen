a77b292b0ef2d483cacf424814643f9ca707b0d1
Joe Richards <nospam-github@disconformity.net>
2014-05-21 23:15:27 -0700

    Version bump to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nospam-github@disconformity.net'
 license          'GPL v3'
 description      'Installs/Configures Tizen Build Tools and Tizen Wearable SDK'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.2.0'
 
 depends          'yum'
 depends          'java'
